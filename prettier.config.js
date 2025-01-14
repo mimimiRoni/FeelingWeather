0b831b87d0305081b0e9ec7bad692cf15d2190c4
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-01-15 08:02:46 +0900

    ﻿[add] ESLintのワークフロー追加 (#3)
    * [add] ESLintのワークフロー追加
    
    * [update] 設定調整
    
    * [update] フォーマット

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,6 @@
-module.exports = {
-  semi: true, // 文末にセミコロンを追加
-  singleQuote: true, // シングルクォートを使用
-  trailingComma: "es5", // ES5の範囲で末尾にカンマを追加
-  tabWidth: 2, // インデントに使うスペースの数
-  printWidth: 80, // 1行の最大文字数
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
 };
