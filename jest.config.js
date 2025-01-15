a0ec33a8c8a70081467415d3ff5c38db380cd53c
mimimiRoni <nakanemiu@gmail.com>
2025-01-15 21:17:35 +0900

    [update] 各設定ファイルをCommonJSにする必要があったので変更
    テストでエラーが出たのでCommonJSに変換、リネーム

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export const preset = 'react';
-export const transform = {
-  '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-};
-export const moduleFileExtensions = ['js', 'jsx', 'ts', 'tsx'];
-export const setupFilesAfterEnv = ['@testing-library/jest-dom/extend-expect'];
