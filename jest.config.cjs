0c00b538bd164477143fdc5852bb6febd747d8ae
mimimiRoni <nakanemiu@gmail.com>
2025-01-16 19:25:34 +0900

    [update] テストレポートを出力するように設定

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,4 +6,8 @@ module.exports = {
   setupFilesAfterEnv: [],
   collectCoverage: true,
   coverageReporters: ['text'],
+  reporters: [
+    'default',
+    ['jest-html-reporters', { publicPath: './test-report' }],
+  ],
 };
