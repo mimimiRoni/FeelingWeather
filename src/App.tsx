84720ce5080fab305be3f99be7bb1c8af9be0a94
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-01-18 18:47:17 +0900

    [update] 都市名の入力フォームとそれを表示する部分を表示 (#19)
    * [update] 都市名の入力フォームとそれを表示する部分を表示
    
    * [update] エラー表示も追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import './App.css';
+import InputCity from './Components/InputCity';
 import Weather from './Components/Weather';
 
 /**
@@ -8,9 +10,21 @@ import Weather from './Components/Weather';
 function App() {
   // TODO: 今は決め打ちで固定値を設定しておくので、気温を取得して入れるようにする
   const temperature = 25;
+  const [getCity, setCity] = useState('都市名');
+  const [getError, setError] = useState<string | null>(null);
 
   return (
     <>
+      <InputCity
+        onSubmit={(city) => {
+          setCity(city);
+          setError(null);
+        }}
+        onError={(errorMassage) => {
+          setError(errorMassage);
+        }}
+      />
+      <p>{getError ? getError : getCity}</p>
       <Weather temperature={temperature} />
     </>
   );
---
+++ src/Components/InputCity.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+type InputCityProps = {
+  onSubmit: (city: string) => void;
+  onError: (errorMassage: string) => void;
+};
+
+const InputCity: React.FC<InputCityProps> = ({ onSubmit, onError }) => {
+  const [inputValue, setInput] = useState('');
+  const onSubmitHandle = (e: React.FormEvent) => {
+    e.preventDefault();
+    const inputStr = inputValue.trim();
+    if (!inputStr) {
+      onError('都市名を入力してください');
+      return;
+    }
+
+    onSubmit(inputStr);
+  };
+
+  return (
+    <form onSubmit={onSubmitHandle} role="form">
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="都市名を入力してください"
+      />
+    </form>
+  );
+};
+
+export default InputCity;
