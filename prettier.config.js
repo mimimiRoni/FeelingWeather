fb914b1ab0e49f9b584d0a153b70ce37243b36fc
mimimiRoni <nakanemiu@gmail.com>
2025-01-18 10:36:36 +0900

    [update] 設定調整

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,10 @@ export default {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true, // オブジェクトリテラルの波括弧内にスペースを追加
+  jsxSingleQuote: false, // JSXでダブルクォートを使用
+  arrowParens: 'always', // アロー関数のパラメータに括弧を追加
+  endOfLine: 'lf',
 };
