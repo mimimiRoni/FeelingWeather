8608ac694299add947efb96f7b14263c206a344c
homi <87790194+mimimiRoni@users.noreply.github.com>
2025-01-25 15:03:56 +0900

    [update] APIリクエストを減らすためにlocalStrageを使うように変更 (#25)

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -7,8 +7,44 @@ export const getCurrentWeather = async (
   lat: number,
   lon: number,
 ): Promise<CurrentWeather> => {
+  const storeKey = `currentWeather-lat${lat}-lon${lon}`;
+  const storedData = localStorage.getItem(storeKey);
+  const now = Date.now();
+
+  if (storedData) {
+    const convertedStoreData = JSON.parse(storedData) as StoreData;
+    if (convertedStoreData !== null) {
+      const diffMinutes =
+        (now - convertedStoreData.storedDate.getTime()) / 60000;
+      if (diffMinutes < 30) {
+        return convertedStoreData.data;
+      } else {
+        localStorage.removeItem(storeKey);
+      }
+    }
+  }
+
   const response = await axios.get<CurrentWeather>(
-    `${functionsUrl}/getCurrentWeather/?lat=${lat}&lon=${lon}`,
+    `${functionsUrl}/getCurrentWeather/`,
+    {
+      params: {
+        lat,
+        lon,
+      },
+    },
   );
+
+  const newStoreData: StoreData = {
+    data: response.data,
+    storedDate: new Date(now),
+  };
+
+  localStorage.setItem(storeKey, JSON.stringify(newStoreData));
+
   return response.data;
+};
+
+type StoreData = {
+  data: CurrentWeather;
+  storedDate: Date;
 };
